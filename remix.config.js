c890fdcc2b675656dc6578894c82c7e8e01765b6
Marco Sansoni <marco.sansoni@sesamecare.com>
2022-04-15 21:10:40 +0200

    Edit remix config

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,15 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  // cacheDirectory: "./node_modules/.cache/remix",
+  cacheDirectory: "./node_modules/.cache/remix",
   // publicPath: "/build/",
   // assetsBuildDirectory: "public/build",
   // serverBuildTarget: "vercel",
+  serverBuildTarget: "vercel",
+  assetsBuildDirectory: "public/build",
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
