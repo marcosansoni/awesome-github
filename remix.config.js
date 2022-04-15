d90ac7d5c0c4a6985de4f9dbec583e50b6018d0e
Marco Sansoni <45231575+marcosansoni@users.noreply.github.com>
2022-04-15 21:23:08 +0200

    Fix styles for deploy on Vercel (#4)
    * Fix styles
    
    * Edit remix config
    
    * Remove config
    
    * Edit remix config
    
    * Add server and update dependencies

--- remix.config.js
+++ remix.config.js
@@ -3,5 +3,14 @@
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
+  // publicPath: "/build/",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildTarget: "vercel",
+  serverBuildTarget: "vercel",
+  assetsBuildDirectory: "public/build",
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
