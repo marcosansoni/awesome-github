60d5a983f72dd8b327eece5a3e51a7d07f811dd1
Marco Sansoni <marco.sansoni@sesamecare.com>
2022-04-15 21:07:09 +0200

    Remove config

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,9 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
-  publicPath: "/build/",
-  assetsBuildDirectory: "public/build",
-  serverBuildTarget: "vercel",
+  // cacheDirectory: "./node_modules/.cache/remix",
+  // publicPath: "/build/",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildTarget: "vercel",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
